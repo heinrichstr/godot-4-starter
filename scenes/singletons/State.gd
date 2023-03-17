7eb1204f37d1f9a8d00397a11292737ede9d4f99
Thomas Heinrichs <tom.heinri@gmail.com>
2023-03-17 13:56:12 -0400

    begin writing settings config save/load

--- scenes/singletons/State.gd
+++ scenes/singletons/State.gd
@@ -1,14 +1,46 @@
 extends Node2D
 
-
+var config:ConfigFile
 var player_input = true
 
 var settings = {
-	"audio": {
-		"masterVolume": 1,
-		"soundVolume": 1,
-		"musicVolume": 1
-	}
+	"audio_masterVolume": 1,
+	"audio_soundVolume": .5,
+	"audio_musicVolume": .25
+}
+
+var settingDefaults = {
+	"audio_masterVolume": 1,
+	"audio_soundVolume": 1,
+	"audio_musicVolume": 1
 }
 
 var state = {}
+
+
+func _ready():
+	load_config()
+
+
+func write_config():
+	pass
+	#todo
+
+
+func load_config():
+	var dir = DirAccess.open("user://settings.cfg")
+	
+	prints(dir)
+	
+	if dir:
+		for configSettings in config.get_sections():
+			print(configSettings)
+	
+	else:
+		#if no config, write defaults
+		
+		config = ConfigFile.new()
+		for setting in settingDefaults:
+			prints("writing default config: ", setting, " ", settingDefaults[setting])
+			config.set_value("settings", setting, settingDefaults[setting])
+	
